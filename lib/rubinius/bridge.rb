430948be50bfb0b2d2c0b5af3c2caf02bb88ae0c
Brian Shirai <brixen@gmail.com>
2013-08-02 22:16:08 -0700

    More migrations.

--- lib/rubinius/bridge.rb
+++ lib/rubinius/bridge.rb
@@ -6,6 +6,7 @@ unless RedCard.check :rubinius
   require "rubinius/bridge/array"
   require "rubinius/bridge/string"
   require "rubinius/bridge/rubinius"
+  require "rubinius/bridge/iseq"
   require "rubinius/bridge/executable"
   require "rubinius/bridge/lookup_table"
   require "rubinius/bridge/tuple"
--- lib/rubinius/bridge/rubinius.rb
+++ lib/rubinius/bridge/rubinius.rb
@@ -1,6 +1,9 @@
 module Rubinius
   Config = { 'eval.cache' => false }
 
+  class CompiledCode
+  end
+
   def synchronize(obj)
     yield
   end
